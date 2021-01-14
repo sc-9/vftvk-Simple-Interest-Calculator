852ac6e4a737df41c29d4adf7549e3d376b8c6a8
sc-9 <siricshekar09@gmail.com>
2021-01-14 00:36:05 -0600

    Simple Interest Calculator

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = p * (rate/100) * years;
+    var d = new Date().getFullYear() + parseInt(years);
+    var res = document.getElementById("result");
+    res.innerHTML = `
+    <p>If you deposit <mark>${p}</mark>,</p>
+    <p>at an interest rate of <mark>${rate}%</mark></p>
+    <p>You will receive an amount of <mark>${interest}</mark></p>
+    <p>in the year <mark>${d}</mark></p>
+    `
 }
         
+
+function input() {
+    var rate = document.getElementById("rate");
+    var output = document.getElementById("percentVal");
+    output.innerHTML = rate.value + "%";
+
+    rate.oninput = function() {
+        output.innerHTML = this.value+ "%";
+    }
+}
