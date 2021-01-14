83a11958c09373e321182c847aaa9a4d424d43a3
sc-9 <siricshekar09@gmail.com>
2021-01-14 01:01:40 -0600

    Final commit

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 function compute()
 {
     var p = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-
-    var interest = p * (rate/100) * years;
-    var d = new Date().getFullYear() + parseInt(years);
-    var res = document.getElementById("result");
-    res.innerHTML = `
-    <p>If you deposit <mark>${p}</mark>,</p>
-    <p>at an interest rate of <mark>${rate}%</mark></p>
-    <p>You will receive an amount of <mark>${interest}</mark></p>
-    <p>in the year <mark>${d}</mark></p>
-    `
+    if(p <= 0 ) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    else {
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+    
+        var interest = p * (rate/100) * years;
+        var d = new Date().getFullYear() + parseInt(years);
+        var res = document.getElementById("result");
+        res.innerHTML = `
+        <p>If you deposit <mark>${p}</mark>,</p>
+        <p>at an interest rate of <mark>${rate}%</mark></p>
+        <p>You will receive an amount of <mark>${interest}</mark></p>
+        <p>in the year <mark>${d}</mark></p>
+        `
+    }
 }
         
 
